cfde4a6a70e01e5146fd46e2d816aeeb6b9177a7
Eric Osmundson <eric.osmundson@gmail.com>
2024-04-17 00:35:58 -0500

    Added a default path if one not provided.

--- p4_obliterate_missing.py
+++ p4_obliterate_missing.py
@@ -10,87 +10,93 @@ Make sure to make the script executable before running.
 '''
 import sys
 import os
-import re
 from P4 import P4, P4Exception
 
 p4 = P4()
 
-try:
-    # Set p4 environment variables
-    p4.port = os.environ['P4PORT']
-    p4.user = os.environ['P4USER']
-    #p4.client = "client-eosmundson"
-
-    # Connect to p4 server
-    p4.connect()
-    # Run p4 verify command and get the number of file results that are stored as dicts in a list
-    verified = p4.run("verify", sys.argv[1])
-    res = len([ele for ele in verified if isinstance(ele, dict)])
-
-    # Set "counter" to zero as that will determine which verified file dictionary is being iterated through
-    # Set "missing" variable to keep track of whether or not any missing revisions have been found.
-
-    count = 0
-    missing = False
-    missing_files = []
-    while count < int(res):
-        if verified[count].get("status") == None:
-            count += 1
-            continue
-        # If the file is missing, store the file and revision in a string and prompt user to obliterate the file. If user enters "yes" to obliterate, then run "p4 obliterate" command.
-        if verified[count]["status"] == "MISSING!":
-            if missing == False: print("Found at least one missing revision.\n")
-            missing = True
-            problem_file = str(verified[count]["depotFile"] + "#" + verified[count]["rev"])
-            # print(problem_file)
-            missing_files.append(problem_file)
-            count += 1
-            continue
-            #answer = input("Do you want to obliterate the missing revision: yes or no? ")
-            #if answer == "yes" or "y":
-            #    p4.run_obliterate(problem_file)
-            #    p4.run("verify", problem_file)
-            #    print(problem_file, "obliterated.")
-            #    count += 1
-            #    continue
-            #else:
-            #    print(problem_file, "not obliterated.")
-            #    count += 1
-            #    continue
-
-    # if no missing file revisions have been found print a message to let the user know.
-    if missing == False:
-        sys.exit("No missing file revisions were found.")
-
-    # Preview a list of files and their revision numbers to be obliterated.
-    view_missing = input("Do you want to see a list of missing files? Y/N: ").lower()
-    if view_missing == "yes" or view_missing == "y":
-        print()
-        for file in missing_files:
-            print(f"{file}")
-            continue
-        print()
-
-    else:
-        pass
-
-    # Obliterate missing file revisions
-    oblit_missing = input("Are you ready to obliterate missing file revisions? Y/N: ").lower()
-    if oblit_missing == "yes" or oblit_missing == "y":
-        for file in missing_files:
-            p4.run_obliterate("-y", "-a", file)
-            # p4.run("verify", file)
-            continue
+def main():
+    try:
+        # Set p4 environment variables
+        p4.port = os.environ['P4PORT']
+        p4.user = os.environ['P4USER']
         
-        print("\nFile revisions have been obliterated.")
+
+        # Connect to p4 server
+        p4.connect()
         
-    else:
-        print("\n\nWARNING: None of the missing file revisions have been obliterated.\n")
+        # Check command-line argument for provided depot path. Set a default P4 depot path to search if one is not provided in the command.
+        if len(sys.argv) == 1:
+            depot_path = '//depot/...'    
+        else:
+            depot_path = sys.argv[1]
+            
+        if not depot_path.endswith('/...'):
+            depot_path = depot_path.rstrip( '/') + '/...'
+            
+        print(f"Looking for librarian errors here:\n{depot_path}")
+                
+        # Run p4 verify command and get the number of file results that are stored as dicts in a list.
+        verified = p4.run("verify", depot_path)
+        res = len([ele for ele in verified if isinstance(ele, dict)])
 
-# Catch and print any P4 exceptions that are raised.
-except P4Exception:
-    for e in p4.errors:
-        print(e)
+        # Set "counter" to zero as that will determine which verified file dictionary is being iterated on.
+        # Create boolean "missing" to keep track of whether or not any missing revisions have been found. If not then the program will exit after 'p4 verify' completes. Boolean value will start as 'False'.
 
-finally:
-    p4.disconnect()
+        counter = 0
+        missing = False
+        missing_files = []
+        while counter < int(res):
+            if verified[counter].get("status") == None:
+                counter += 1
+                continue
+            # If the file is missing, store the file and revision as a string in the 'missing_files' list.
+            if verified[counter]["status"] == "MISSING!":
+                # The first time a missing revision is found, script will print a notification.
+                if missing == False: print("Found at least one missing revision.\n")
+                # Since a missing revision has been found, change boolean value to 'True'.
+                missing = True
+                # Concatenate depotFile name and revision number and store in missing_files list. Increase counter by one and move to next file in verified list
+                problem_file = str(verified[counter]["depotFile"] + "#" + verified[counter]["rev"])
+                missing_files.append(problem_file)
+                counter += 1
+                continue
+                
+
+        # If no missing file revisions have been found print a message to let the user know and exit the script.
+        if missing == False:
+            sys.exit("No missing file revisions were found.")
+
+        # Preview a list of files and their revision numbers to be obliterated.
+        view_missing = input("Do you want to see a list of missing files? Y/N: ").lower()
+        if view_missing == "yes" or view_missing == "y":
+            print()
+            for file in missing_files:
+                print(f"{file}")
+                continue
+            print()
+
+        else:
+            pass
+
+        # Obliterate missing file revisions
+        oblit_missing = input("Are you ready to obliterate missing file revisions? Y/N: ").lower()
+        if oblit_missing == "yes" or oblit_missing == "y":
+            for file in missing_files:
+                p4.run_obliterate("-y", "-a", file)
+                continue
+            # Print successful obliteration of revisions.
+            print("\nFile revisions have been obliterated.")
+            
+        else:
+            print("\n\nWARNING: None of the missing file revisions have been obliterated.\n")
+
+    # Catch and print any P4 exceptions that are raised.
+    except P4Exception:
+        for e in p4.errors:
+            print(e)
+
+    finally:
+        p4.disconnect()
+
+if __name__ == "__main__":
+    main()
